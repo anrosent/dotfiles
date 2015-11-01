5fa1143ca36c6c4d18b8a23964de7531baf894d4
anrosent <anson.rosenthal@gmail.com>
2015-10-31 23:48:31 -0400

    improving .vimrc

--- src/.vim/.init.zsh
+++ src/.vim/.init.zsh
@@ -1,19 +1,19 @@
 #!/bin/zsh
 RDIR=$(dirname $0)
-
 PLUGIN_REPOS_F=$RDIR/repos.txt
+
 mkdir -p $RDIR/bundle
 
 while read line;
 do
-    echo ".vim: Dependency $line"
-    if [ ! -d "bundle/$(basename ${line%.*})" ];
+    DST="$RDIR/bundle/$(basename ${line%.*})"
+    if [ ! -d $DST ];
     then
-        echo "cloning $line into .vim"
+        echo ".vim: cloning dependency $line into $DST"
         pushd $RDIR/bundle
         git clone $line
         popd
     else
-        echo "Dependency already satisfied"
+        echo ".vim: Dependency $line already satisfied"
     fi
 done < $PLUGIN_REPOS_F
